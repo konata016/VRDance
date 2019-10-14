fb181b975f1b418ba169c2d38b72c245b3526ca4
Unknown <konata016@gmail.com>
2019-10-14 09:30:24 +0900

    シェーダーグラフ入り

---
+++ GameTitle/Assets/my/Scripts/konata/ScanShaderTiming.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScanShaderTiming : MonoBehaviour
+{
+
+    public Material material;
+    float barTime;
+    bool onMusicStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //一小節の時間の計算
+        //60*拍子*小節数/テンポ
+        barTime = 60 * Music.MyInspectorList[0].UnitPerBeat * 1 / (float)Music.MyInspectorList[0].Tempo;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!onMusicStart)
+        {
+            if (Music.IsPlaying && Music.IsJustChangedBar())
+            {
+                //曲が始まったら光の棒が生成される
+                material.SetFloat("Vector1_156873A5", 1f);
+
+                //光の棒が出るまでの時間を調整したい（1小節ごとに）
+                material.SetFloat("Vector1_C34C010D", barTime);
+                onMusicStart = true;
+            }
+        }
+    }
+}
