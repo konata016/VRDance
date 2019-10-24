3a7095e03fd385ab96eb98dc2e23619b21d62906
d <d>
2019-10-24 17:19:43 +0900

    更新

--- GameTitle/Assets/my/Scripts/konata/Notes/NotesManager.cs
+++ GameTitle/Assets/my/Scripts/konata/Notes/NotesManager.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//両サイド作ってから判定の処理を書くこと
+//インデックス外になったらNotesMove()のところ
+//ループを両サイドで書けば直るはず
 
 public class NotesManager : MonoBehaviour
 {
@@ -35,10 +36,9 @@ public class NotesManager : MonoBehaviour
         SpawnNotes();
         NotesMove();
         RankJudge();
-        overNotes();
+        OverNotes();
     }
 
-    //ノーツ生成処理
     void SpawnNotes()
     {
         if (Music.IsPlaying)
@@ -46,9 +46,10 @@ public class NotesManager : MonoBehaviour
             if (Music.IsJustChangedBar())
             {
                 //左右のノーツ生成
-                notesRightList.Add(Spawn(new Vector3(0, 90, 0)));
-                notesLeftList.Add(Spawn(new Vector3(0, -90, 0)));
+                notesRightList.Add(Spawn(45));
+                notesLeftList.Add(Spawn(-45));
 
+                //リストの最後を取得するための計算
                 int maxRight = notesRightList.Count - 1;
                 int maxLeft = notesLeftList.Count - 1;
 
@@ -59,16 +60,25 @@ public class NotesManager : MonoBehaviour
             else if (Music.IsJustChangedBeat())
             {
                 //左右のノーツ生成
-                notesRightList.Add(Spawn(new Vector3(0, 90, 0)));
-                notesLeftList.Add(Spawn(new Vector3(0, -90, 0)));
+                notesRightList.Add(Spawn(45));
+                notesLeftList.Add(Spawn(-45));
             }
         }
 
         //ノーツの生成
-        GameObject Spawn(Vector3 roll)
+        GameObject Spawn(float roll)
         {
-            GameObject obj = Instantiate(notesObj, transform.position, Quaternion.Euler(roll)) as GameObject;
+            //Y軸上にroll分だけ回転させる
+            Quaternion rot = Quaternion.AngleAxis(roll, Vector3.up);
+            //自信の回転取得
+            Quaternion q = transform.rotation;
+
+            //初期設定の回転と自信の回転を掛け合わせたら、うまいこと合成できたぜ！
+            GameObject obj = Instantiate(notesObj, transform.position, q * rot) as GameObject;
+
+            //自信の子オブジェクトとして登録する
             obj.transform.transform.parent = transform;
+
             return obj;
         }
 
@@ -96,32 +106,39 @@ public class NotesManager : MonoBehaviour
         //動かす式
         Quaternion Move(GameObject obj, int direction)
         {
-            Vector3 v3 = obj.transform.rotation.eulerAngles;
+            //動きの計算
+            float move = direction * speed * Time.deltaTime;
+            //Y軸を基準としてmove分だけ回転する
+            Quaternion rot = Quaternion.AngleAxis(move, Vector3.up);
+            //生成したオブジェクトの回転データ
+            Quaternion q = obj.transform.rotation;
 
-            v3.y += direction * speed * Time.deltaTime;
-            return Quaternion.Euler(v3);
+            //掛け合わせることによって、生成オブジェクト独自の動きと
+            //親オブジェクトの回転を合成することができる
+            return q * rot;
         }
     }
 
     //ノーツがオーバーしていればノーツを消す
-    void overNotes()
+    void OverNotes()
     {
-        if (over(notesLeftList))
+        if (Over(notesLeftList))
         {
             DestroyNotes(notesRightList);
             DestroyNotes(notesLeftList);
         }
 
         //ノーツがオーバーしているかどうかの判定
-        bool over(List<GameObject> objList)
+        bool Over(List<GameObject> objList)
         {
             //リストがないときには実行しないようにする
             if (objList.Count != 0)
             {
-                Vector3 roll = objList[0].transform.rotation.eulerAngles;
+                //生成したオブジェクトのローカル回転データ格納
+                Vector3 roll = objList[0].transform.localRotation.eulerAngles;
 
                 //回転したときに360以上になると0になってしまうため消す範囲を制限する必要があった
-                if (roll.y < 10 && roll.y > 3) return true;
+                if (roll.y < 10 && roll.y > 1) return true;
                 else return false;
             }
             else return false;
@@ -134,10 +151,11 @@ public class NotesManager : MonoBehaviour
         //リストがないときには実行しないようにする
         if (notesLeftList.Count != 0)
         {
-            Vector3 roll = notesLeftList[0].transform.rotation.eulerAngles;
+            //生成したオブジェクトのローカル回転データ格納
+            Vector3 roll = notesLeftList[0].transform.localRotation.eulerAngles;
 
             //トリガーがオンになったら実行
-            if (onTrigger())
+            if (OnTrigger())
             {
                 //ランクの判定
                 if (roll.y > butPos.y && roll.y <= gootPos.y)
@@ -167,7 +185,7 @@ public class NotesManager : MonoBehaviour
     }
 
     //トリガーの処理をここに入れる
-    bool onTrigger()
+    bool OnTrigger()
     {
         if (StepDetermination.isGroundTouch_L == StepDetermination.ISGROUNDTOUCH.Landing ||
             StepDetermination.isGroundTouch_R == StepDetermination.ISGROUNDTOUCH.Landing ||
@@ -175,7 +193,24 @@ public class NotesManager : MonoBehaviour
         {
             return true;
         }
-        else return false;     
+        else return false;
+
+        //ボタンの処理
+        bool OnDebugKey()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha0) ||
+                Input.GetKeyDown(KeyCode.Alpha1) ||
+                Input.GetKeyDown(KeyCode.Alpha2) ||
+                Input.GetKeyDown(KeyCode.Alpha3) ||
+                Input.GetKeyDown(KeyCode.Alpha4) ||
+                Input.GetKeyDown(KeyCode.Alpha5) ||
+                Input.GetKeyDown(KeyCode.Alpha6) ||
+                Input.GetKeyDown(KeyCode.Alpha7))
+            {
+                return true;
+            }
+            else return false;
+        }
     }
 
     //ノーツを消す処理
@@ -184,21 +219,5 @@ public class NotesManager : MonoBehaviour
         GameObject obj = objList[0];
         objList.RemoveAt(0);
         Destroy(obj);
-    }
-
-    bool OnDebugKey()
-    {
-        if (Input.GetKeyDown(KeyCode.Alpha0) ||
-            Input.GetKeyDown(KeyCode.Alpha1) ||
-            Input.GetKeyDown(KeyCode.Alpha2) ||
-            Input.GetKeyDown(KeyCode.Alpha3) ||
-            Input.GetKeyDown(KeyCode.Alpha4) ||
-            Input.GetKeyDown(KeyCode.Alpha5) ||
-            Input.GetKeyDown(KeyCode.Alpha6) ||
-            Input.GetKeyDown(KeyCode.Alpha7))
-        {
-            return true;
-        }
-        else return false;
     }
 }
