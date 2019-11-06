d36a284bf977429e45fe0529f32fc29516c8f0cf
d <d>
2019-11-06 19:20:11 +0900

    更新

---
+++ GameTitle/Assets/my/Scripts/konata/Test/PlAction/Triangle/TriangleAttack/EnemyPosSynchronize.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPosSynchronize : MonoBehaviour
+{
+   public Vector3 fixPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localPosition = GameObject.Find("EnemyPoint").transform.position + fixPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
