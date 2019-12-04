9050c1a925eb2b1499620275e2bf9a358a89909a
d <d>
2019-12-04 22:34:21 +0900

    更新

--- GameTitle/Assets/my/Scripts/konata/Notes/WaveMateriaAttachl.cs
+++ GameTitle/Assets/my/Scripts/konata/Notes/WaveMateriaAttachl.cs
@@ -5,45 +5,39 @@ using UnityEngine;
 public class WaveMateriaAttachl : MonoBehaviour
 {
     public Vector3[] pos = new Vector3[4];
-    public Material[] materialArr = new Material[3];
-    public Material material0;
-    public Material m;
-    public Renderer mesh;
+    public GameObject[] shaderObj = new GameObject[3];
     bool onStart;
 
     // Start is called before the first frame update
     void Start()
     {
-        m = mesh.materials[1];
+        //m = GetComponent<Renderer>().materials[0];
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!onStart)
+        for (int i = 0; i < transform.childCount; i++)
         {
-            Vector3 v3 = transform.position;
+            Vector3 v3 = transform.GetChild(i).gameObject.transform.position;
             if (v3.z < pos[1].z)
             {
-                gameObject.GetComponent<MeshRenderer>().materials[1] =material0;
+                //デザさんにお願いしてMaterialの順番を変える
+
+                Material[] materials = transform.GetChild(i).gameObject.GetComponent<Renderer>().materials;
+                materials[1] = shaderObj[0].GetComponent<Renderer>().materials[1];
+                transform.GetChild(i).gameObject.GetComponent<Renderer>().materials = materials;
             }
-            //for (int i = 0; i < transform.childCount; i++)
-            //{
-            //    //Vector3 v3 = transform.GetChild(i).gameObject.transform.position;
-            //    //if (v3.z < pos[1].z)
-            //    //{
-            //        GetComponent<Renderer>().materials[1] = materialArr[0];
-            //    //}
-            //    //else if (pos[1].z > pos[2].z && pos[2].z <= pos[3].z)
-            //    //{
-            //    //    transform.GetChild(i).gameObject.GetComponent<Renderer>().materials[1] = materialArr[1];
-            //    //}
-            //    //else if (pos[2].z > pos[3].z)
-            //    //{
-            //    //    transform.GetChild(i).gameObject.GetComponent<Renderer>().materials[1] = materialArr[2];
-            //    //}
-            //}
-            onStart = true;
+            else if (v3.z <= pos[1].z && v3.z > pos[2].z)
+            {
+
+            }
+
         }
+    }
+
+    void SetMaterial()
+    {
+
     }
 }
