50e2ae87fb6fd5a28e1b6f42b6305c64761ad3ec
Unknown <konata016@gmail.com>
2019-12-04 11:52:42 +0900

    更新

--- GameTitle/Assets/my/Scripts/konata/Notes/WaveMateriaAttachl.cs
+++ GameTitle/Assets/my/Scripts/konata/Notes/WaveMateriaAttachl.cs
@@ -4,30 +4,46 @@ using UnityEngine;
 
 public class WaveMateriaAttachl : MonoBehaviour
 {
-    public Vector3[] pos = new Vector3[5];
-    public Material[] materialArr=new Material[3];
-    List<GameObject> objList = new List<GameObject>();
+    public Vector3[] pos = new Vector3[4];
+    public Material[] materialArr = new Material[3];
+    public Material material0;
+    public Material m;
+    public Renderer mesh;
+    bool onStart;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        m = mesh.materials[1];
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (pos[0].z > pos[1].z && pos[1].z <= pos[2].z)
+        if (!onStart)
         {
-
-        }
-        else if (pos[1].z > pos[2].z && pos[2].z <= pos[3].z)
-        {
-
-        }
-        else if (pos[3].z > pos[4].z)
-        {
-
+            Vector3 v3 = transform.position;
+            if (v3.z < pos[1].z)
+            {
+                gameObject.GetComponent<MeshRenderer>().materials[1] =material0;
+            }
+            //for (int i = 0; i < transform.childCount; i++)
+            //{
+            //    //Vector3 v3 = transform.GetChild(i).gameObject.transform.position;
+            //    //if (v3.z < pos[1].z)
+            //    //{
+            //        GetComponent<Renderer>().materials[1] = materialArr[0];
+            //    //}
+            //    //else if (pos[1].z > pos[2].z && pos[2].z <= pos[3].z)
+            //    //{
+            //    //    transform.GetChild(i).gameObject.GetComponent<Renderer>().materials[1] = materialArr[1];
+            //    //}
+            //    //else if (pos[2].z > pos[3].z)
+            //    //{
+            //    //    transform.GetChild(i).gameObject.GetComponent<Renderer>().materials[1] = materialArr[2];
+            //    //}
+            //}
+            onStart = true;
         }
     }
 }
