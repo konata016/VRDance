53dd9e17a6a1f168189f6d0211f15f2bd1438ffb
d <d>
2019-12-09 22:20:02 +0900

    更新

---
+++ GameTitle/Assets/my/Scripts/konata/Field/WorldColorChange.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class WorldColorChange : MonoBehaviour
+{
+    public Color color;
+    public PostProcessVolume post;
+    ColorGrading colorGrading;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(PostProcessEffectSettings item in post.profile.settings)
+        {
+            if(item as ColorGrading)
+            {
+                colorGrading = item as ColorGrading;
+            }
+        }
+
+        if (colorGrading)
+        {
+            colorGrading.colorFilter.value= color;
+        }
+    }
+}
---
+++ GameTitle/Assets/my/Scripts/konata/Field/WorldColorChange1.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldColorChange1 : MonoBehaviour
+{
+    public float speed = 5;
+    public Material badMaterial;
+    public Material goodMaterial;
+    public Material excellentMaterial;
+
+    float alpha;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        alpha = excellentMaterial.color.a;
+
+        Color color = GetComponent<Renderer>().material.color;
+        color.a = 0;
+        GetComponent<Renderer>().material.color = color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OnTrigger())
+        {
+            switch (NotesManager2.rank)
+            {
+                case NotesManager2.RANK.Bad: GetComponent<Renderer>().material = badMaterial; break;
+                case NotesManager2.RANK.Good: GetComponent<Renderer>().material = goodMaterial; break;
+                case NotesManager2.RANK.Excellent: GetComponent<Renderer>().material = excellentMaterial; break;
+                default:break;
+            }
+            Color color= GetComponent<Renderer>().material.color;
+            color.a = alpha;
+            GetComponent<Renderer>().material.color = color;
+        }
+
+
+        if(0< GetComponent<Renderer>().material.color.a)
+        {
+            Color color = GetComponent<Renderer>().material.color;
+            color.a -= speed * Time.deltaTime;
+            GetComponent<Renderer>().material.color = color;
+        }
+
+    }
+
+    bool OnTrigger()
+    {
+        bool on = Input.GetKeyDown(KeyCode.Alpha3);
+        return on;
+    }
+}
