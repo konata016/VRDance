4b0e8b7ba897bb93a673dbf518eca415e1e2b95e
Unknown <konata016@gmail.com>
2019-12-10 01:56:45 +0900

    エディター追加

---
+++ GameTitle/Assets/my/Scripts/konata/Debug/SceneChange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public KeyCode keyCode = KeyCode.E;
+    public string scene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyCode))
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
