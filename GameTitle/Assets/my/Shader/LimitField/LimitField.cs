e96b05936254a6f5ccff265fd0c4decafb39c811
d <d>
2019-12-17 20:46:34 +0900

    範囲制限の表示入れた

---
+++ GameTitle/Assets/my/Shader/LimitField/LimitField.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimitField : MonoBehaviour
+{
+    public Material material;
+    public GameObject limitFieldObj;
+    float dis;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dis = transform.position.z - limitFieldObj.transform.position.z;
+        material.SetFloat("_Move", Mathf.Abs(dis));
+    }
+}
