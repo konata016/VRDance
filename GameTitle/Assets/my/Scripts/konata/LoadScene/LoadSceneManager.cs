96212ed5cafe87131d921234bfd923a990e1e67c
d <d>
2020-01-15 16:33:46 +0900

    LoadSceneあと移動だけ

---
+++ GameTitle/Assets/my/Scripts/konata/LoadScene/LoadSceneManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSceneManager : MonoBehaviour
+{
+    public GameObject panel;
+    public GameObject foot;
+
+    public static bool GetEndProcess { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dis = Vector3.Distance(panel.transform.position, foot.transform.position);
+
+        if (dis < 0.1f)
+        {
+            GetEndProcess = true;
+        }
+        else
+        {
+            GetEndProcess = false;
+        }
+    }
+}
---
+++ GameTitle/Assets/my/Scripts/konata/LoadScene/UiChange.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiChange : MonoBehaviour
+{
+    public GameObject processNow;
+    public GameObject processEnd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        processEnd.SetActive(false);
+        processNow.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (LoadSceneManager.GetEndProcess)
+        {
+            processNow.SetActive(false);
+            processEnd.SetActive(true);
+        }
+        else
+        {
+            processEnd.SetActive(false);
+            processNow.SetActive(true);
+        }
+    }
+}
