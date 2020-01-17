babfe21ec2a4797e3176504e9a4cc378bbd767b1
Unknown <konata016@gmail.com>
2020-01-18 00:11:53 +0900

    撮影用

---
+++ GameTitle/Assets/my/Scripts/konata/Debug/Look.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Look : MonoBehaviour
+{
+    Vector3 roll;
+    public float speed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            roll.y += speed * Time.deltaTime;
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            roll.y -= speed * Time.deltaTime;
+        }
+        transform.eulerAngles = roll;
+
+    }
+}
