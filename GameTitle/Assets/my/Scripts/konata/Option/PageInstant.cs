3c04e8c6e6684001167ef341781941e6384a3661
d <d>
2020-01-21 22:23:59 +0900

    途中

---
+++ GameTitle/Assets/my/Scripts/konata/Option/PageInstant.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PageInstant : MonoBehaviour
+{
+    public GameObject pageNumObj;
+    public float space = 0.2f;
+    public int instantCount = 7;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < instantCount; i++)
+        {
+            if (instantCount % 2 == 0)
+            {
+
+            }
+            else
+            {
+                
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GameTitle/Assets/my/Scripts/konata/Option/PageNum.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PageNum : MonoBehaviour
+{
+    public Material on;
+    public Material off;
+
+    GameObject[] numArr;
+    int num;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //子オブジェクトを検出し、ページオブジェクトとして扱う
+        numArr = new GameObject[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            numArr[i] = transform.GetChild(i).gameObject;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //切り替え
+        if (OnTriggerNext() && num < numArr.Length - 1) num++;
+        if (OnTriggerBack() && num > 0) num--;
+
+        //トリガーがひかれた場合、マテリアルの差し替え
+        if (OnTriggerNext() || OnTriggerBack())
+        {
+            for (int i = 0; i < numArr.Length; i++)
+            {
+                numArr[i].GetComponent<Renderer>().material = off;
+            }
+            numArr[num].GetComponent<Renderer>().material = on;
+        }
+    }
+
+    bool OnTriggerNext()
+    {
+        return Input.GetKeyDown(KeyCode.RightArrow);
+    }
+
+    bool OnTriggerBack()
+    {
+        return Input.GetKeyDown(KeyCode.LeftArrow);
+    }
+}
