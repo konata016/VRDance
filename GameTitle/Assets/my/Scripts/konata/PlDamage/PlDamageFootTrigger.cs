30edd8d1e51360bf0ab8825964fad2d91156688c
d <d>
2020-01-30 16:47:13 +0900

    当たり判定追加

--- GameTitle/Assets/my/Scripts/konata/PlDamage/PlDamageFootTrigger.cs
+++ GameTitle/Assets/my/Scripts/konata/PlDamage/PlDamageFootTrigger.cs
@@ -2,28 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// 敵の攻撃にコリジョンが接触した場合、ダメージを受けたという信号を送る
-/// </summary>
-public class PlDamageTrigger : MonoBehaviour
+public class PlDamageFootTrigger : MonoBehaviour
 {
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (!PlDamageStage.GetNoDamageTrigger)
         {
-            if (other.gameObject.tag == "wave" || other.gameObject.tag == "throw")
+            if (other.gameObject.tag == "underWave")
             {
                 PlDamageStage.OnDamageTrigger = true;
 
